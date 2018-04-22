cb231299574faaf361a74d93065ccc53ee01df98
caiahnbin <1850445175@qq.com>
2018-04-22 21:32:01 +0800

    sort-v4

--- src/com/chapter4/Sort.java
+++ src/com/chapter4/Sort.java
@@ -4,25 +4,41 @@ import java.util.Arrays;
 
 /**
  * 排序专题
- * 复杂度为O(nlogn);
- * 1.归并排序
- * 2.快速排序
- * 3.桶排
- * 4.希尔排序
- * 复杂度为O(n*n)
- * 1.冒泡排序
- * 2.选择排序
- * 3.插入排序
+ * 复杂度为O(n*n);1.冒泡 2.选择 3.插入排序
+ * 复杂度为O(nlogn); 1.归并排序 2.快速排序 3.堆排 4.希尔排序
+ * 复杂度为O(n);1.计数排序 2.基数排序
  * @author caihanbin
  *
  */
 public class Sort {
 	public static void main(String[] args){
-		int[] A=new int[]{2,1,4,5,3,3,1,2,9,4};
+		int[] A=new int[]{73,22, 93, 43, 55, 14, 28, 65, 39, 81,99};
 		//quick_sort(A,0,9);
-		heapSort(A,9);
-		System.out.println(Arrays.toString(A));
+		int max=getMaxNumLen(A);
+		int[] arr=shellSort(A,max);
+		System.out.println(max+" "+Arrays.toString(arr));
 	}
+	 /**
+	   * 插入排序
+	   * @param A
+	   * @param length
+	   */
+	public static void insertSort(int[] A,int length){
+		
+		for(int i=1;i<length;i++){
+			int key=A[i];
+			int j=i-1;	
+			while(j>=0&&key<A[j]){
+				A[j+1]=A[j];
+				j--;
+				A[j+1]=key;
+			}
+			
+		}
+		
+		
+	}
+	
 	/**
 	 * 归并排序
 	 * @param A
@@ -144,13 +160,117 @@ public class Sort {
 	  * @param n
 	  * @return
 	  */
-	 public int[] shellSort(int[] A, int n) {
-	        // write code here
-		 return null;
+	 public static int[] shellSort(int[] A, int n) {
+	   
+	     int feet=A.length/2;
+	     int index=0;
+	     while(feet>0){
+	    	 for(int i=feet;i<A.length;i++){
+	    		 index=i;
+	    		 while(index-feet>=0){
+	    			 
+	    			 if(A[index-feet]>A[index]){
+	    				swap(A,index-feet,index);
+	    				index-=feet;
+	    			 }else{
+	    				 break;
+	    			 }
+	    		 }
+	    		 
+	    		 
+	    	 }
+	    	 
+	    	 feet/=2;
+	    	 
+	     }
+	     
+		 return A;
 	  }
 	 
+	 /**
+	  * 计数排序 
+	  * @param A
+	  * @param n
+	  * @return
+	  */
 	 
+	 public static int[] countingSort(int[] A, int n) {
+		 int min=A[0];
+		 int max=A[0];
+		 for(int i=1;i<A.length;i++){
+			 min=Math.min(A[i],min);
+			 max=Math.max(A[i],max);
+		 }
+		 //统计每一个格子对应多少个数字
+		 int[] countArr=new int[max-min+1];
+		 for(int i=0;i<A.length;i++){
+			 countArr[A[i]-min]++;
+		 }
+		 int index=0;
+		 for(int i=0;i<countArr.length;i++){
+			 while(countArr[i]-->0){
+				 A[index++]=i+min;
+			 }
+		 }
+		 return A;
+	    
+	    }
 	 
+	 /**
+	  * 基数排序
+	  * @param A
+	  * @param n
+	  * @return
+	  */
+	 public static int[] radixSort(int[] A, int d) {
+		 int n=1;
+		 int k=0;
+		 int length=A.length;
+		 int[][] bucket=new int[10][length];
+		 int[] order=new int[10];
+		 while(n<d){
+			 for(int num:A){
+				 int digit=(num/n)%10;
+				 bucket[digit][order[digit]]=num;
+				 order[digit]++;
+			 }
+			 for(int i=0;i<10;i++){
+				 if(order[i]!=0){
+					 for(int j=0;j<order[i];j++){
+						 A[k]=bucket[i][j];
+						 k++;
+					 }
+					 
+					 
+				 }
+				 order[i]=0;
+			 }
+			 n*=10;
+			 k=0;
+			 
+		 }
+		 return A; 
+	    }
+	 	//获取数组最大数位
+		public static int getMaxNumLen(int[] A){
+			int max=A[0];
+			for(int i=1;i<A.length;i++){
+				if(A[i]>max){
+					max=A[i];
+				}
+			}
+			int temp=1;
+			while(true){
+				temp*=10;
+				if(max/temp==0){
+					break;
+				}
+				
+			}
+			
+			return temp;
+		}
+ 
 	 
 	 public static void swap(int[] A,int n,int m){
 		 int temp=A[n];
